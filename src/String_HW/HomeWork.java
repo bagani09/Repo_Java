e69d0327c9d531549fe1433f83229ca8eb0ae3ba
Ani B <anita@anis-mbp.home>
2019-12-23 20:27:21 -0500

    Class Work

--- src/String_HW/HomeWork.java
+++ src/String_HW/HomeWork.java
@@ -1,4 +1,16 @@
 package String_HW;
 
+
 public class HomeWork {
+    public static void main(String[] args) {
+        int age = 31;
+        boolean isChild = age <= 7;
+        boolean isSenior = age >= 65;
+        System.out.println(isChild);
+        System.out.println(isSenior);
+
+        
+
+}
+
 }
