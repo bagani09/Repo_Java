f4fbfd7dd955b0cf0aec2ba25ff2b9d7a4241370
Ani B <anita@Anis-MacBook-Pro.local>
2019-12-30 12:30:33 -0500

    First homework

--- src/String_HW/HomeWork.java
+++ src/String_HW/HomeWork.java
@@ -16,62 +16,62 @@ public class HomeWork {
         //        Celsius to Fahrenheit
         //        T(°F) = T(°C) × 9/5 + 32
         double fahrenheit = (celsius) * 9/5 + 32;
-        System.out.println(fahrenheit);
+        System.out.println("Temperature value after changing from Celsius to Fahrenheit:" + " " + fahrenheit);
 
 //        Celsius to Kelvin
 //        T(K) = T(°C) + 273.15
         double kelvin = (celsius) + 273.15;
-        System.out.println(kelvin);
+        System.out.println("Temperature value after changing from Celsius to Kelvin:" + " " + kelvin);
 
 //        Celsius to Rankine
 //        T(°R) = (T(°C) + 273.15) × 9/5
         double rankine = ((celsius) + 273.15) * 9/5;
-        System.out.println(rankine);
+        System.out.println("Temperature value after changing from Celsius to Rankine:" + " " + rankine);
 
 //        Fahrenheit to Celsius
 //        T(°C) = (T(°F) - 32) × 5/9
         double FtoC = ((fahrenheit) - 32) * 5/9;
-        System.out.println(FtoC);
+        System.out.println("Temperature value after changing from Fahrenheit to Celsius:" + " " + FtoC);
 
 //        Fahrenheit to Kelvin
 //        T(K) = (T(°F) + 459.67)× 5/9
         double FtoK = ((fahrenheit) + 459.67) * 5/9;
-        System.out.println(FtoK);
+        System.out.println("Temperature value after changing from Fahrenheit to Kelvin:" + " " + FtoK);
 
 //        Fahrenheit to Rankine
 //        T(°R) = T(°F) + 459.67
         double FtoR = (fahrenheit) + 459.67;
-        System.out.println(FtoR);
+        System.out.println("Temperature value after changing from Fahrenheit to Rankine:" + " " + FtoR);
 
 //        Kelvin to Fahrenheit
 //        T(°F) = T(K) × 9/5 - 459.67
         double KtoF = (kelvin) * 9/5 - 459.67;
-        System.out.println(KtoF);
+        System.out.println("Temperature value after changing from Kelvin to Fahrenheit:" + " " + KtoF);
 
 //        Kelvin to Celsius
 //        T(°C) = T(K) - 273.15
         double KtoC = (kelvin) - 273.15;
-        System.out.println(KtoC);
+        System.out.println("Temperature value after changing from Kelvin to Celsius:" + " " + KtoC);
 
 //        Kelvin to Rankine
 //        T(°R) = T(K) × 9/5
         double KtoR = (kelvin) * 9/5;
-        System.out.println(KtoR);
+        System.out.println("Temperature value after changing from Kelvin to Rankine:" + " " + KtoR);
 
 //        Rankine to Fahrenheit
 //        T(°F) = T(°R) - 459.67
         double RtoF = (rankine) - 459.67;
-        System.out.println(RtoF);
+        System.out.println("Temperature value after changing from Rankine to Fahrenheit:" + " " + RtoF);
 
 //        Rankine to Celsius
 //        T(°C) = (T(°R) - 491.67) × 5/9
         double RtoC = ((rankine) - 491.67) * 5/9;
-        System.out.println(RtoC);
+        System.out.println("Temperature value after changing from Rankine to Celsius:" + " " + RtoC);
 
 //        Rankine to Kelvin
 //        T(K) = T(°R) × 5/9
         double RtoK = (rankine) * 5/9;
-        System.out.println(RtoK);
+        System.out.println("Temperature value after changing from Rankine to kelvin:" + " " + RtoK);
 
 
 
